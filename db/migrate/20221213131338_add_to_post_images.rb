6656a3a8a021c8c51ba8164c15714273b0a19e43
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-13 13:43:29 +0000

    add 12

---
+++ db/migrate/20221213131338_add_to_post_images.rb
@@ -0,0 +1,4 @@
+class AddToPostImages < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_131023) do
+ActiveRecord::Schema.define(version: 2022_12_13_131338) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
